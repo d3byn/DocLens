8b0a45d5a4450f92f96ab4653690621d93b380ea
d3byn <d3bayansarkar@gmail.com>
2026-09-14 02:35:55 +0530

    Updated extract function

--- lib/extract.ts
+++ lib/extract.ts
@@ -1,16 +1,17 @@
-import { PDFParse } from "pdf-parse";
+import { extractText as extractPdfText, getDocumentProxy } from "unpdf";
 
 export async function extractText(file: File): Promise<string> {
-    const buffer = Buffer.from(await file.arrayBuffer());
-    if (file.type === "application/pdf") {
-        const parser = new PDFParse({ data: buffer });
-        const result = await parser.getText();
-        await parser.destroy();
+  const buffer = new Uint8Array(await file.arrayBuffer()); //Uint8Array stores unsigned 8bit int
 
-        return result.text;
-    }
-    if (file.type === "text/plain") {
-        return buffer.toString("utf-8");
-    }
-    throw new Error("Unsupported file type. Please upload a PDF or TXT file.");
+  if (file.type === "application/pdf") {
+    const pdf = await getDocumentProxy(buffer);
+    const { text } = await extractPdfText(pdf, { mergePages: true });
+    return text;
+  }
+
+  if (file.type === "text/plain") {
+    return Buffer.from(buffer).toString("utf-8");
+  }
+
+  throw new Error("Unsupported file type. Please upload a PDF or TXT file.");
 }
